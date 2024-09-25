9cb02aa4922fcbfb55cb859a45353f979151cb24
Wellington <wellington.bassani28@gmail.com>
2024-09-24 21:40:09 -0300

    AULA10

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Retangulo re1 = new Retangulo();
+Retangulo re2 = new Retangulo(5);
+Retangulo re3 = new Retangulo(4,6);
+
+Console.WriteLine($"Retangulo 1 Area: {re1.CalcularArea()} e Perimetro: {re1.CalcularPerimetro()}"); 
+Console.WriteLine($"Retangulo 2 Area: {re2.CalcularArea()} e Perimetro: {re2.CalcularPerimetro()}"); 
+Console.WriteLine($"Retangulo 3 Area: {re3.CalcularArea()} e Perimetro: {re3.CalcularPerimetro()}"); 
+
+re3.Redimensionar(2);
+Console.WriteLine($"Retangulo 3 Area Redimensionada: {re3.CalcularArea()} e Perimetro {re3.CalcularPerimetro()}");
+re2.Redimensionar(10, 15);
+Console.WriteLine($"Retangulo 2 Area Redimensionada: {re2.CalcularArea()} e Perimetro {re2.CalcularPerimetro()}");
---
+++ Retangulo.cs
@@ -0,0 +1,46 @@
+public class Retangulo
+{
+    double largura;
+    double altura;
+
+    public Retangulo()
+    {
+        largura = 0;
+        altura = 0;
+    }
+
+    public Retangulo(double valor)
+    {
+        altura = valor;
+        largura = valor;
+    }
+
+    public Retangulo(double largura, double altura)
+    {
+        this.altura = altura;
+        this.largura = largura;
+    }
+    public double CalcularArea()
+    {
+        return largura * altura;
+    }
+    public double CalcularPerimetro()
+    {
+        return (largura + altura) * 2;
+    }
+    public void Redimensionar()
+    {
+
+    }
+    public void Redimensionar(double fator)
+    {
+        altura *= fator;
+        largura *= fator; 
+    }
+    public void Redimensionar(double altura, double largura)
+    {
+        this.altura = altura;
+        this.largura = largura;
+    }
+
+}
